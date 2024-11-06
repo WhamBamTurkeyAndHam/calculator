e8ed0f941e99309b75b938e28759e0e369358921
WhamBamTurkeyAndHam <benji.olsen6@gmail.com>
2024-11-06 16:54:17 +1300

    Add handles for numbers and operators
    Add the handles needed to handle and use numbers and operators
    Fixed operate function to correctly use numbers and operators

--- js/index.js
+++ js/index.js
@@ -9,17 +9,51 @@ let operators = document.querySelectorAll('.operators');
 const decimal = document.querySelector('.decimal');
 const equal = document.querySelector('.equal');
 
-let firstNumber = '';
-let secondNumber = '';
+let previousNumber = '';
+let currentNumber = '';
+let operator = '';
 
-//Calculate
+//Handle numbers that are selected.
+numbers.forEach(numbers => numbers.addEventListener('click',(e) => {
+  handleNumbers(e.target.textContent);
+  bottomDisplay.textContent = currentNumber;
+}));
+
+function handleNumbers(num) {
+  currentNumber += num;
+};
+
+//Handle operators that are selected.
+operators.forEach(op => op.addEventListener('click',(e) => {
+  handleOperators(e.target.textContent);
+  topDisplay.textContent = `${previousNumber} ${operator}`;
+  bottomDisplay.textContent = '';
+}));
+
+function handleOperators(op) {
+  operator = op
+  previousNumber = currentNumber;
+  currentNumber = '';
+};
+
+//Calculate.
 equal.addEventListener('click', () => {
   operate();
-})
+});
 
 function operate() {
-  return  operators === '+' ? firstNumber += secondNumber 
-        : operators === '-' ? firstNumber -= secondNumber 
-        : operators === 'X' ? firstNumber *= secondNumber 
-        : firstNumber /= secondNumber;
+  previousNumber = Number(previousNumber);
+  currentNumber = Number(currentNumber);
+
+  //Ternary style if else type statement.
+  previousNumber = operator === '+' ? previousNumber + currentNumber 
+                  : operator === '-' ? previousNumber - currentNumber 
+                  : operator === 'X' ? previousNumber * currentNumber 
+                  : currentNumber !== 0 ? previousNumber / currentNumber
+                  : 'ERROR: Division by Zero'
+
+  bottomDisplay.textContent = previousNumber;
+  currentNumber = previousNumber;
+  operator = '';
+  return previousNumber;
 };
